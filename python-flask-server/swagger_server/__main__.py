31acb133aeab3e730956c0ec43bc574a10b63fa6
Geordon Worley <vadixidav@gmail.com>
2017-02-26 13:00:38 -0500

    force localhost

--- python-flask-server/swagger_server/__main__.py
+++ python-flask-server/swagger_server/__main__.py
@@ -8,5 +8,5 @@ if __name__ == '__main__':
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'API developed for the pathfinding algorithm'})
-    app.run(host='ec2-52-41-229-1.us-west-2.compute.amazonaws.com', port=8080)
+    app.run(host='localhost', port=8000)
 
